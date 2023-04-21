1dc45dd8918a97af94c248a78f540ab09d025b1b
divya-rai-42 <lci2020025@iiitl.ac.in>
2023-04-22 00:34:36 +0530

    Added files

---
+++ script.py
@@ -0,0 +1,173 @@
+from flask import Flask, request, render_template, jsonify, Response, send_file
+import os
+# check opencv version
+import cv2
+# print version number
+print(cv2.__version__)
+import os
+from matplotlib import pyplot
+from matplotlib.patches import Rectangle
+from matplotlib.patches import Circle
+from mtcnn.mtcnn import MTCNN
+import face_recognition
+import shutil
+import zipfile
+
+app = Flask(__name__)
+
+# Set the upload folder and allowed file types
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png', 'gif'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 200 * 1024 * 1024
+
+# Check if a file has a permitted file extension
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def createZip():
+    folder_path = "./Segregated_folders"
+    zip_path = "./Segregated_folders.zip"
+
+    with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for root, dirs, files in os.walk(folder_path):
+            for file in files:
+                zipf.write(os.path.join(root, file))
+    print("zip folder created")
+
+
+def processImages():
+    filenames = os.listdir(app.config['UPLOAD_FOLDER'])
+
+    folder_name = "Segregated_folders"
+
+    # Specify the path where you want to create the new folder
+    path = "./"
+
+    # Use the os.mkdir() method to create the new folder
+    os.mkdir(os.path.join(path, folder_name))
+
+    # Verify that the folder has been created
+    if os.path.exists(os.path.join(path, folder_name)):
+        print("Folder created successfully!")
+    else:
+        print("Folder creation failed.")
+
+    rootdir = './uploads'
+    segregated_dir = './Segregated_folders'
+    for file in os.listdir(rootdir):
+        filename = os.path.join(rootdir, file)
+        # load image from file
+        pixels = pyplot.imread(filename)
+        # create the detector, using default weights
+        detector = MTCNN()
+        # detect faces in the image
+        faces = detector.detect_faces(pixels)
+        # display faces on the original image
+        # draw_faces(filename, faces)
+        # print(faces)
+        print("Number of faces:" + str(len(faces)))
+        for face in faces:
+            file_path = "./this_face.jpg"
+            x1, y1, width, height = face['box']
+            x2, y2 = x1 + width, y1 + height
+            cv2.imwrite(file_path,pixels[y1:y2, x1:x2])
+            list_dir = os.listdir(segregated_dir)
+
+            flag = False
+            flag2 = False
+
+            for item in list_dir:
+                item_path = os.path.join(segregated_dir, item)
+
+                primary_path = item_path + "/primary.jpg"
+
+                print(primary_path)
+
+                if os.path.exists(primary_path) == False:
+                    continue
+
+                img = cv2.imread(primary_path)
+                rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                # img_encoding = face_recognition.face_encodings(img)[0]
+                face_locations = face_recognition.face_locations(img, number_of_times_to_upsample=2)
+                face_encodings_sample = face_recognition.face_encodings(img, known_face_locations=face_locations, num_jitters=100)
+                if len(face_encodings_sample) == 0:
+                    flag2 = True
+                    break
+                face_encodings = face_recognition.face_encodings(img, known_face_locations=face_locations, num_jitters=100)[0]
+
+                img2 = cv2.imread("./this_face.jpg")
+                rgb_img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
+                # img_encoding2 = face_recognition.face_encodings(img2)[0]
+                face_locations2 = face_recognition.face_locations(img2, number_of_times_to_upsample=2)
+                face_encodings2_sample = face_recognition.face_encodings(img2, known_face_locations=face_locations2, num_jitters=100)
+
+                if len(face_encodings2_sample) == 0:
+                    flag2 = True
+                    break
+                face_encodings2 = face_recognition.face_encodings(img2, known_face_locations=face_locations2, num_jitters=100)[0]
+
+                results = face_recognition.compare_faces([face_encodings], face_encodings2,0.5)
+
+                if results[0] == True:
+                    flag = True
+                    shutil.copy(filename, item_path)
+                    break
+
+
+            
+            if flag2 == True:
+                continue
+
+            if flag == False:
+                number = len(list_dir)
+                new_folder_name = "Person" + str(number)
+
+                # Specify the path where you want to create the new folder
+                new_path = "./Segregated_folders/"
+
+                # Use the os.mkdir() method to create the new folder
+                new_folder_path = os.path.join(new_path, new_folder_name)
+                os.mkdir(new_folder_path)
+
+                cv2.imwrite(new_folder_path + "/primary.jpg",pixels[y1:y2, x1:x2])
+                shutil.copy(filename, new_folder_path)
+
+            
+            if os.path.exists(file_path):
+                os.remove(file_path)
+                print(f"{file_path} has been deleted.")
+            else:
+                print(f"{file_path} does not exist.")
+
+
+
+
+
+
+# Render the web form
+@app.route('/')
+def upload_form():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def upload():
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part in the request'}), 400
+
+    files = request.files.getlist('file')
+
+    for file in files:
+        # save each file to the upload folder
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+    
+    processImages()
+    createZip()
+    # return Response('File uploaded successfully', status=200)
+    return send_file("./Segregated_folders.zip", as_attachment=True)
+
+if __name__ == "__main__":
+    app.run(debug = True)
